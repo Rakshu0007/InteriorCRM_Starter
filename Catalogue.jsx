e3d2649d27057b309d9ba7ebae88c8d0134ec65e
Rakshu0007 <rakshurakshirh23@gmail.com>
2025-07-12 17:57:52 +0000

    Catalogue added

---
+++ Catalogue.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Catalogue() {
+  return (
+    <div>
+      <h2 className="text-2xl font-semibold mb-4">Catalogue</h2>
+      <p>Product and material catalogue.</p>
+    </div>
+  );
+}
