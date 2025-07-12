48e67e564ab81bca3f81494eda8d3e77f7ef4aca
Rakshu0007 <rakshurakshirh23@gmail.com>
2025-07-12 18:05:47 +0000

    BOQ Added

---
+++ BOQ.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function BOQ() {
+  return (
+    <div>
+      <h2 className="text-2xl font-semibold mb-4">Bill of Quantities (BOQ)</h2>
+      <p>Dynamic quote builder, versioning, exports.</p>
+    </div>
+  );
+}
