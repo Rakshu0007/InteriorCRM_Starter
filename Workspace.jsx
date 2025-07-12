3fde3d6c2e97e23f28b2831507dc4ce2ee364158
Rakshu0007 <rakshurakshirh23@gmail.com>
2025-07-12 10:49:00 +0000

    Work space added

---
+++ Workspace.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Workspace() {
+  return (
+    <div>
+      <h2 className="text-2xl font-semibold mb-4">Workspace</h2>
+      <p>List of leads with name, mobile, last update, stage, etc.</p>
+    </div>
+  );
+}
