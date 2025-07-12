67e9529b8cf58ca54687a99f71be70a2c1a16402
Rakshu0007 <rakshurakshirh23@gmail.com>
2025-07-12 18:00:03 +0000

    Timeliine added

---
+++ Timeline.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Timeline() {
+  return (
+    <div>
+      <h2 className="text-2xl font-semibold mb-4">Timeline</h2>
+      <p>Project phases, tasks, deadlines.</p>
+    </div>
+  );
+}
